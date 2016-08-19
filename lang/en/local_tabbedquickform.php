d83bc66380432f576812bd5ea4c7431cac081010
valery <valery.fremaux@gmail.com>
2016-08-19 22:32:24 +0200

    fix grouped elements

--- lang/en/local_tabbedquickform.php
+++ lang/en/local_tabbedquickform.php
@@ -1,12 +1,35 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * @package   local_tabbedquickform
+ * @category  blocks
+ * @author    Valery Fremaux (valery.fremaux@gmail.com)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 $string['tabbedquickform:canswitchfeatured'] = 'Can switch full and filtered features';
 
 $string['pluginname'] = 'Tabbed QuickForms';
 $string['enterconfigure'] = 'Enter form tuning';
 $string['exitconfigure'] = 'Exit form tuning';
-$string['filterfeatures'] = 'Back to simple mode';
-$string['fullfeatured'] = 'Full featured';
+$string['filterfeatures'] = 'Enter simple mode';
+$string['fullfeatured'] = 'Enter full featured';
 $string['simple'] = 'Simple';
 $string['complete'] = 'Complete';
 $string['hasmaskeditems'] = 'This form has masked items';
