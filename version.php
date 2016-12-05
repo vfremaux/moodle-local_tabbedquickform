e1e47f56b86f1d35fbe6a352cb90f87d4ac291ab
valery <valery.fremaux@gmail.com>
2016-12-05 15:20:26 +0100

    Automated sync from SVN

--- version.php
+++ version.php
@@ -26,11 +26,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016080600;
+$plugin->version = 2016120500;
 $plugin->requires = 2014110400;
 $plugin->component = 'local_tabbedquickform';
-$plugin->maturity = MATURITY_RC;
-$plugin->release = '2.8.0';
+$plugin->maturity = MATURITY_BETA;
+$plugin->release = '2.8.0 (Build 2016120500)';
 
 // Non moodle attributes.
 $plugin->codeincrement = '2.8.0000';
