5540718829ab48bd68a57721d592552008e1f37a
valery <valery.fremaux@gmail.com>
2020-07-11 15:44:56 +0200

    Automated sync from SVN

--- lib.php
+++ lib.php
@@ -31,5 +31,59 @@ defined('MOODLE_INTERNAL') || die();
  */
 function local_tabbedquickform_extend_navigation() {
     global $PAGE;
+}
 
+function local_tabbedquickform_hook() {
+    global $PAGE, $CFG;
+
+    if (!during_initial_install()) {
+        include_once($CFG->dirroot.'/local/tabbedquickform/QuickForm_Extensions/MoodleForm_Tabbed_Renderer.php');
+        $config = get_config('local_tabbedquickform');
+
+        if (empty($config->enable)) {
+            // Wuick trap if not enabled.
+            return;
+        }
+
+        if (defined('AJAX_SCRIPT') && AJAX_SCRIPT) {
+            // We cannot tune forms that are invoked through AJAX XHR calls.
+            return;
+        }
+
+        $exclusions = [
+            'page-mod-tracker-reportissue',
+            'page-local-vmoodle-view',
+            'page-admin-tool-mnetusers*',
+            'page-admin-user-user_bulk',
+            'page-mod-data-export',
+            'page-admin-purgecaches',
+            'page-admin-user',
+            'page-admin-search',
+            'page-admin-tool-usertour',
+            'page-local-userenrols-import'
+        ];
+
+        // Process eventual exclusions administrators have identified.
+        $additionalexclusions = explode("\n", trim(@$config->excludepagetypes));
+        if (!empty($additionalexclusions)) {
+            foreach ($additionalexclusions as $exc) {
+                if (!in_array(trim($exc), $exclusions)) {
+                    $exclusions[] = trim($exc);
+                }
+            }
+        }
+
+        $excluded = false;
+        foreach ($exclusions as $exc) {
+            if (!empty($exc)) {
+                $exc = str_replace('*', '.*', trim($exc));
+                $exc = str_replace('?', '.', $exc);
+                $excluded = $excluded || preg_match("/$exc/", $PAGE->bodyid);
+            }
+        }
+
+        if (!$excluded) {
+            $GLOBALS['_HTML_QuickForm_default_renderer'] = new MoodleQuickForm_Tabbed_Renderer();
+        }
+    }
 }
